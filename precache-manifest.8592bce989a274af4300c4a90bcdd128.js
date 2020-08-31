7f4e973fc8dc3343f56c3a90b71ed568cf968923
Nalin28 <nalinmunshi@gmail.com>
2020-09-01 00:48:10 +0530

    Updates

---
+++ precache-manifest.8592bce989a274af4300c4a90bcdd128.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c623e01ee948ed384d49f5a8273d2988",
+    "url": "/React-Resume/index.html"
+  },
+  {
+    "revision": "a3ed81bc0db455901cf8",
+    "url": "/React-Resume/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "34a080678b377a294501",
+    "url": "/React-Resume/static/css/main.6ab6415c.chunk.css"
+  },
+  {
+    "revision": "a3ed81bc0db455901cf8",
+    "url": "/React-Resume/static/js/2.a9523c8f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Resume/static/js/2.a9523c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a080678b377a294501",
+    "url": "/React-Resume/static/js/main.d6d13783.chunk.js"
+  },
+  {
+    "revision": "63779cac7e3c31fcafaa",
+    "url": "/React-Resume/static/js/runtime-main.5725c6e8.js"
+  },
+  {
+    "revision": "5261c31e3e53bbabb7bfe42004f4bfab",
+    "url": "/React-Resume/static/media/BenefitEveryone.5261c31e.png"
+  },
+  {
+    "revision": "2803b34ee08bf452853c4743387f558c",
+    "url": "/React-Resume/static/media/ConnectEverything.2803b34e.png"
+  }
+]);
