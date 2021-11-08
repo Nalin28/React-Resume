1461c00312517bb7378d322ecc85bda2c9a2e297
Nalin28 <nmunshi@student.ubc.ca>
2021-11-07 16:38:44 -0800

    added another certificate

--- src/components/Awards/Awards.js
+++ src/components/Awards/Awards.js
@@ -3,11 +3,12 @@ import { AwardsObject } from "../../Constants";
 import { UncontrolledCarousel } from "reactstrap";
 import bene from "../../assets/BenefitEveryone.png";
 import conn from "../../assets/ConnectEverything.png";
+import conn2 from "../../assets/connect_everything_award.png";
 
 const awards = () => {
   return (
     <div className="Awards">
-      <UncontrolledCarousel items={[{ src: bene }, { src: conn }]} />
+      <UncontrolledCarousel items={[{ src: bene }, { src: conn }, {src: conn2}]} />
     </div>
   );
 };
