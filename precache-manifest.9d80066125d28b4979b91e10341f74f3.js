7173667bb577b0b8832a0588ddab27e45b6afc34
Nalin28 <nmunshi@student.ubc.ca>
2022-08-23 21:13:30 -0700

    Updates

---
+++ precache-manifest.9d80066125d28b4979b91e10341f74f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc34e44bceda92da98ef9cb9d0fe7d8f",
+    "url": "/React-Resume/index.html"
+  },
+  {
+    "revision": "ce116f13ff9cd105c151",
+    "url": "/React-Resume/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "218e0b4bd56f753fbfb8",
+    "url": "/React-Resume/static/css/main.3772c118.chunk.css"
+  },
+  {
+    "revision": "ce116f13ff9cd105c151",
+    "url": "/React-Resume/static/js/2.2e4913ec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Resume/static/js/2.2e4913ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218e0b4bd56f753fbfb8",
+    "url": "/React-Resume/static/js/main.65fa20cc.chunk.js"
+  },
+  {
+    "revision": "63779cac7e3c31fcafaa",
+    "url": "/React-Resume/static/js/runtime-main.5725c6e8.js"
+  },
+  {
+    "revision": "5261c31e3e53bbabb7bfe42004f4bfab",
+    "url": "/React-Resume/static/media/BenefitEveryone.5261c31e.png"
+  },
+  {
+    "revision": "2803b34ee08bf452853c4743387f558c",
+    "url": "/React-Resume/static/media/ConnectEverything.2803b34e.png"
+  },
+  {
+    "revision": "713c01aeb029265ef5f00e5fc0098db2",
+    "url": "/React-Resume/static/media/connect_everything_award.713c01ae.png"
+  },
+  {
+    "revision": "2d4902698dec9ce6528b8aca364a4426",
+    "url": "/React-Resume/static/media/nm.2d490269.png"
+  }
+]);
